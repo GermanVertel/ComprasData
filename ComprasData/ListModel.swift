e1bdaf0533c9b2f9fb6554d7d306f7e4fa9757ed
german vertel <gvertel88@gmail.com>
2024-06-26 18:19:29 -0500

    Creacion de un nuevo archivo para la creacion de la entidad

--- ComprasData/ListModel.swift
+++ ComprasData/ListModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import SwiftData
+
+// creacion de la entidad
+
+
+@Model
+final class ListModel {
+    var titulo : String
+    var fecha : Date
+    var completado : Bool
+    var presupuesto : String
+    var total : Float
+    
+    init(titulo: String, fecha: Date, completado: Bool, presupuesto: String, total: Float) {
+        self.titulo = titulo
+        self.fecha = fecha
+        self.completado = completado
+        self.presupuesto = presupuesto
+        self.total = total
+    }
+    
+    
+    
+}
